aaccd7325ba03d145069e4e0b7b49ffe37f30f1e
Andrew Corum <14coruma@gmail.com>
2020-07-28 12:06:33 -0400

    * Working flying enemy. LevelManager reads txt file in Resources folder for waves

---
+++ Assets/Scripts/EnemyFlyAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlyAI : EnemyAI
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player").GetComponent<Player>();
+        target = GameObject.Find("Stop").transform;
+        healthBar = transform.Find("HealthBar").GetComponent<HealthBar>();
+
+        health = maxHealth;
+        healthBar.UpdateMax(maxHealth);
+        healthBar.UpdateValue(health);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector2 direction = ((Vector2) target.position - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        rb.AddForce(force);
+
+        distance = Vector2.Distance(rb.position, target.position);
+
+        if (rb.velocity != Vector2.zero) {
+            transform.Find("EnemySprite").right = rb.velocity.normalized;
+        }
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,35 +7,42 @@ public class LevelManager : MonoBehaviour
     public float width = 17f;
     public float height = 7f;
     Enemies enemies;
-    public Transform enemyPrefab;
-    public string wavesString; // waveString is for easy level editing, parsed into waves
+    public List<string> enemyIds;
+    Dictionary<string,Transform> enemyPrefabs;
+    string wavesString;
     public float enemyDelay = 1f;
     public float waveDelay = 10f;
 
     void Start() {
         enemies = GameObject.Find("Enemies").GetComponent<Enemies>();
+        wavesString = Resources.Load<TextAsset>("EnemyText/LevelEnemies0").ToString();
+        enemyPrefabs = new Dictionary<string, Transform>();
+        foreach (string enemyId in enemyIds) {
+            enemyPrefabs.Add(enemyId, Resources.Load<GameObject>("Prefabs/Enemy" + enemyId).transform);
+        }
 
         List<List<object>> waves = ParseWaveString();
         for (int i = 0; i < waves.Count; i++) {
             enemies.SendWave(
-                i, enemyPrefab, (int)waves[i][0], (float)waves[i][1], (int)waves[i][2],
-                waveDelay, enemyDelay
+                i, enemyPrefabs[(string)waves[i][0]], (int)waves[i][1], (float)waves[i][2],
+                (int)waves[i][3], waveDelay, enemyDelay
             );
         }
     }
     
-    /// EX: """5,300,5;7,350,10"""
-    /// Wave 0: 5 enemies, 300f health, 5 points each 
-    /// Wave 1: 7 enemies, 350f health, 10points each
+    /// EX: """0,5,300,5"""
+    /// Enemy0, 5 enemies, 300f health, 5 points each 
     List<List<object>> ParseWaveString() {
         List<List<object>> waves = new List<List<object>>(0);
-        string[] wavesSplit = wavesString.Split(';');
+        string[] wavesSplit = wavesString.Split(
+            new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
         foreach(string s in wavesSplit) {
             string[] sSplit = s.Split(',');
-            List<object> wave = new List<object>(3);
-            wave.Add(int.Parse(sSplit[0]));
-            wave.Add(float.Parse(sSplit[1]));
-            wave.Add(int.Parse(sSplit[2]));
+            List<object> wave = new List<object>(4);
+            wave.Add(sSplit[0]);
+            wave.Add(int.Parse(sSplit[1]));
+            wave.Add(float.Parse(sSplit[2]));
+            wave.Add(int.Parse(sSplit[3]));
             waves.Add(wave);
         }
         return waves;
